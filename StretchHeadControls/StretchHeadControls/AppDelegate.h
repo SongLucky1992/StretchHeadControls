4cf71d04ab5748a7d92f5ff40ebb73a0e5cfc77c
宋好云 <songhaoyun@songhaoyundeMacBook-Pro.local>
2017-11-06 16:51:23 +0800

    可视化头部控件

---
+++ StretchHeadControls/StretchHeadControls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StretchHeadControls
+//
+//  Created by 宋好云 on 2017/11/6.
+//  Copyright © 2017年 宋好云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
